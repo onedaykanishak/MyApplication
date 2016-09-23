cf74ab36b6c75082dbc99a3e2b78ef33f41cbc6d
kanishak <onedaykanishak@gmail.com>
2016-09-23 01:12:42 -0400

    Adding Files
    I am Adding two files in same project. Please check.

---
+++ app/src/main/java/com/example/kanishakrishi/myapplication/newClass.java
@@ -0,0 +1,13 @@
+package com.example.kanishakrishi.myapplication;
+
+/**
+ * Created by kanishakrishi on 2016-09-23.
+ */
+
+public class newClass {
+
+    public void work() {
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+    }
---
+++ app/src/main/java/com/example/kanishakrishi/myapplication/olg.java
@@ -0,0 +1,13 @@
+package com.example.kanishakrishi.myapplication;
+
+/**
+ * Created by kanishakrishi on 2016-09-23.
+ */
+
+public class olg {
+
+    public void work() {
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+}
